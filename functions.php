7c9c5b6598918ad9213cb8a6e343f323766b5b07
Nicolas Fiascaro <n.fiascaro@gmail.com>
2019-12-04 11:02:45 +0100

    feat(breadcrumb) : Yoast custom separator POC

--- functions.php
+++ functions.php
@@ -8,6 +8,10 @@ require_once 'autoload.php';
  * Load all services
  */
 add_action( 'after_setup_theme', function () {
+	// Yoast custom separator Test
+	add_filter( 'wpseo_breadcrumb_separator', function( $separator ){
+		return '<svg class="icon icon-right" focusable="false" aria-hidden="true" role="img"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-right"></use></svg>';
+	} );
 	// Boot the service, at after_setup_theme.
 	\BEA\Theme\Framework\Framework::get_container()->boot_services();
 } );
--- index.php
+++ index.php
@@ -1,8 +1,15 @@
 <?php get_header(); ?>
 <div class="container">
-<?php while ( have_posts() ) {
-	the_post();
-	the_content();
-} ?>
+	<?php
+	if ( function_exists('yoast_breadcrumb') ) {
+		yoast_breadcrumb( '<section class="breadcrumb">','</section>' );
+	}
+	?>
+	<div class="wysiwyg">
+		<?php while ( have_posts() ) {
+			the_post();
+			the_content();
+		} ?>
+	</div>
 </div>
 <?php get_footer(); ?>
